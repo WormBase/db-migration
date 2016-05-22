adb2609f4989c7494085d998e0b24b7c9d8422b8
Matt Russell <matt@mgrbyte.co.uk>
2016-05-23 00:02:15 +0100

    Update setup classifiers.

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Topic :: Software Development :: Libraries :: Python Modules :: Tools',
     ],
