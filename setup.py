10f1a2814e094a051cb5b0c72ea14c8af12c89cf
Matt Russell <matt@mgrbyte.co.uk>
2017-02-16 16:04:22 +0000

    Back to development: 0.23

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ INSTALL_REQUIRES = _read_file('requirements.txt').splitlines()
 
 setup(
     name='azanium',
-    version='0.22',
+    version='0.23.dev0',
     url='http://www.wormmbase.org/',
     author='Matt Russell, EMBL-EBI',
     author_email='matthew.russell@wormbase.org',
