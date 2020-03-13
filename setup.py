417949c7e4a131074b62352f5a74285af37deeba
Matt Russell <matthew.russell@wormbase.org>
2020-03-13 10:26:14 +0000

    Back to development: 0.59

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ INSTALL_REQUIRES = _read_file('requirements.txt').splitlines()
 
 setup(
     name='azanium',
-    version='0.58',
+    version='0.59.dev0',
     url='http://www.wormmbase.org/',
     author='Matt Russell, EMBL-EBI',
     author_email='matthew.russell@wormbase.org',
